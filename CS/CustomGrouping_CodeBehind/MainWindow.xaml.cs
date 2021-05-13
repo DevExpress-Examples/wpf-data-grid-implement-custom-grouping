3368d248f7b17dbdd9da3d1bed7b2ea6d5cd4182
volkov.anton <anton.volkov@devexpress.com>
2021-05-13 18:16:10 +0300

    Update sample - use new Command API

---
+++ CS/CustomGrouping_CodeBehind/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using DevExpress.Xpf.Grid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomGrouping_CodeBehind {
+    public class Person {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int UnitPrice { get; set; }
+
+        public Person() { }
+        public Person(int i) {
+            FirstName = "FirstName" + i;
+            LastName = "LastName" + i;
+            UnitPrice = i;
+        }
+    }
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            grid.ItemsSource = GetPersonList().ToList();
+        }
+        static IEnumerable<Person> GetPersonList() {
+            return Enumerable.Range(0, 100).Select(i => new Person(i));
+        }
+
+        void OnCustomColumnGroup(object sender, CustomColumnSortEventArgs e) {
+            if(e.Column.FieldName != "UnitPrice")
+                return;
+            double x = Math.Floor(Convert.ToDouble(e.Value1) / 10);
+            double y = Math.Floor(Convert.ToDouble(e.Value2) / 10);
+            e.Result = x > 9 && y > 9 ? 0 : x.CompareTo(y);
+            e.Handled = true;
+        }
+        
+        void OnCustomGroupDisplayText(object sender, CustomGroupDisplayTextEventArgs e) {
+            if(e.Column.FieldName != "UnitPrice")
+                return;
+            string interval = IntervalByValue(e.Value);
+            e.DisplayText = interval;
+        }
+        // Gets the text that represents the interval which contains the specified value.
+        private string IntervalByValue(object val) {
+            double d = Math.Floor(Convert.ToDouble(val) / 10);
+            string ret = string.Format("{0:c} - {1:c} ", d * 10, (d + 1) * 10);
+            if(d > 9)
+                ret = string.Format(">= {0:c} ", 100);
+            return ret;
+        }
+    }
+}
---
+++ CS/CustomGrouping_MVVM/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.Xpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CustomGrouping_MVVM {
+    public class Person {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int UnitPrice { get; set; }
+
+        public Person() { }
+        public Person(int i) {
+            FirstName = "FirstName" + i;
+            LastName = "LastName" + i;
+            UnitPrice = i;
+        }
+    }
+
+    public class MainViewModel : ViewModelBase {
+        public ObservableCollection<Person> ListPerson { get; }
+        
+        public MainViewModel() {
+            ListPerson = new ObservableCollection<Person>(GetPersonList());
+        }
+        static IEnumerable<Person> GetPersonList() {
+            return Enumerable.Range(0, 100).Select(i => new Person(i));
+        }
+
+        [Command]
+        public void CustomColumnGroup(RowSortArgs args) {
+            if(args.FieldName != "UnitPrice")
+                return;
+            double x = Math.Floor(Convert.ToDouble(args.FirstValue) / 10);
+            double y = Math.Floor(Convert.ToDouble(args.SecondValue) / 10);
+            args.Result = x > 9 && y > 9 ? 0 : x.CompareTo(y);
+        }
+
+        [Command]
+        public void CustomGroupDisplayText(GroupDisplayTextArgs args) {
+            if(args.FieldName != "UnitPrice")
+                return;
+            string interval = IntervalByValue(args.Value);
+            args.DisplayText = interval;
+        }
+        // Gets the text that represents the interval which contains the specified value.
+        private string IntervalByValue(object val) {
+            double d = Math.Floor(Convert.ToDouble(val) / 10);
+            string ret = string.Format("{0:c} - {1:c} ", d * 10, (d + 1) * 10);
+            if(d > 9)
+                ret = string.Format(">= {0:c} ", 100);
+            return ret;
+        }
+    }
+}
